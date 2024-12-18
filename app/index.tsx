9b35c4b46d0bbee96b3c8c95b7902acbf3b28257
aloysathekge <sathekgealoy@gmail.com>
2024-12-18 08:50:46 +0200

    Refactor Index Screen: Replace welcome message with loading indicator
    - Removed static welcome text and navigation button.
    - Added ActivityIndicator for loading state in the main view.
    - Updated layout to center the loading indicator.

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,17 @@
 import { AppScreenContainer } from "@/components/AppScreenContainer";
 import { ScreenContent } from "@/components/ScreenContent";
 import { useRouter } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { ActivityIndicator, Button, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
   return (
     <AppScreenContainer>
       <ScreenContent>
-        <View style={{ padding: 10 }}>
-          <Text>Hey welcome to Gapiet chats</Text>
-          <Button
-            title="go to welcome"
-            onPress={() => router.push("./welcomeScreen")}
-          />
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" />
         </View>
       </ScreenContent>
     </AppScreenContainer>
