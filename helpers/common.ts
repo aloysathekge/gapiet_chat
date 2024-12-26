fec06d884f0ea97890108414f4d3a0947ecde7a3
aloysathekge <sathekgealoy@gmail.com>
2024-12-26 13:05:43 +0200

    Added dynamic  time formatter

--- helpers/common.ts
+++ helpers/common.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { Dimensions } from "react-native";
 
 export const {width:deviceWidth, height:deviceHeight}=Dimensions.get("window")
@@ -15,3 +16,27 @@ export const stripHtmlTags=(html:any)=>{
     return html.replace(/<[^>]*>?/gm,'')
 }
 
+export const formatTime= (timestamp: string | Date, detailed: boolean = false) => {
+    const momentDate = moment(timestamp);
+    
+    if (detailed) {
+        return momentDate.format("h:mm A · MMM D, YYYY");
+    }
+    
+    const now = moment();
+    const diffMinutes = now.diff(momentDate, 'minutes');
+    const diffHours = now.diff(momentDate, 'hours');
+    const diffDays = now.diff(momentDate, 'days');
+
+    if (diffMinutes < 1) {
+        return 'now';
+    } else if (diffMinutes < 60) {
+        return `${diffMinutes}m`;
+    } else if (diffHours < 24) {
+        return `${diffHours}h`;
+    } else if (diffDays < 7) {
+        return `${diffDays}d`;
+    } else {
+        return momentDate.format('MMM D');
+    }
+}
